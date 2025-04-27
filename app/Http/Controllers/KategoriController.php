4b46db241ae2fcf21cec6c54f3f177e1f9055f6e
azharfahri <fahrikatepp@gmail.com>
2025-04-27 17:00:43 +0700

    mankas

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -12,6 +12,10 @@ class KategoriController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $kategori = kategori::all();
--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -14,6 +14,10 @@ class TransaksiController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $transaksi = transaksi::all();
